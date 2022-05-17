45fc0caae23254f31389cfdba5917a11f6c57c97
Alex Aredov <aaredov@bk.ru>
2022-05-17 21:51:50 +0300

    ref

---
+++ sixteen/2.py
@@ -0,0 +1,12 @@
+def F(n):
+    if n > 0:
+        G(n - 1)
+
+
+def G(n):
+    print("*")
+    if n > 1:
+        F(n - 2)
+
+print(F(11))
+#4
---
+++ sixteen/4.py
@@ -0,0 +1,5 @@
+def F(n):
+    if n == 0:
+        return 0
+    else:
+        if (n > 0) and (n % 2 == 0)
