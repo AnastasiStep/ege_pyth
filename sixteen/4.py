3fc566f55cb64e9aa6cd0604ec062fdc8db0df2d
Alex Aredov <aaredov@bk.ru>
2022-05-17 21:53:27 +0300

    ref

--- sixteen/4.py
+++ sixteen/4.py
@@ -2,4 +2,8 @@ def F(n):
     if n == 0:
         return 0
     else:
-        if (n > 0) and (n % 2 == 0)
+        if (n > 0) and (n % 2 == 0):
+            return F(n//2)
+        if (n > 0) and (n % 2 != 0):
+            return 1 + F(n - 1)
+print(F(100))
