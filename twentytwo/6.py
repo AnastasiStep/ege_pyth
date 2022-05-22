43cd2aae708397ee3663925291f3c9087c6d0121
Alex Aredov <aaredov@bk.ru>
2022-05-23 00:44:12 +0300

    smth

---
+++ twentytwo/6.py
@@ -0,0 +1,10 @@
+for i in range(0,1000):
+    x = i
+    a, b = 0, 0
+    while x > 0:
+        a = a + 1
+        b = b + x%100
+        x = x//100
+    if (a == 2) and (b == 13):
+        print(i, a, b)
+#112
