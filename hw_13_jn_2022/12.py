ae54d9582c8d0d9df129fcb5ff23d548f48ffa56
Alex Aredov <aaredov@bk.ru>
2022-06-12 01:54:09 +0300

    jdcn

---
+++ hw_13_jn_2022/12.py
@@ -0,0 +1,18 @@
+l = 0
+for k in range(1, 10000):
+    n = 1850000000 + k
+    sd = []
+    for i in range(2, int(n**0.5)):
+        if n % i == 0 and i**2 != n:
+            if i % 2 == 0:
+                sd.append(i)
+            if n//i % 2 == 0:
+                sd.append(n//i)
+        if i**2 == n:
+            if i % 2 == 0:
+                sd.append(i)
+    if len(sd) % 2 != 0:
+        l += 1
+        print(k, len(sd))
+    if l == 5:
+        break
