65813d1177db6fdf517f0bf9431fe14a1d854a09
Alex Aredov <aaredov@bk.ru>
2022-06-11 23:45:43 +0300

    njv

---
+++ hw_13_jn_2022/8_2.py
@@ -0,0 +1,16 @@
+def f(s1, s2, n):
+    if (s1 + s2) >= 63 and (s1 + s2) <= 74:
+        if n == 3: return True
+        else: return False
+    elif s1 + s2 > 74:
+        if n == 2: return True
+        else: return False
+    else:
+        if n >= 3: return False
+        elif n % 2 != 0: 
+            return f(s1 + 2, s2, n + 1) and f(s1, s2 + 2, n + 1) and f(s1 * 2, s2, n + 1) and f(s1, s2 * 2, n + 1)
+        else:
+            return f(s1 + 2, s2, n + 1) or f(s1, s2 + 2, n + 1) or f(s1 * 2, s2, n + 1) or f(s1, s2 * 2, n + 1)
+for s in range(1, 48):
+    if f(15, s, 0):
+        print(s)
