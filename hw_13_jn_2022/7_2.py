113753a0ad1da4284e38ae5a003fe351f6c00086
Alex Aredov <59872895+AlexAredov@users.noreply.github.com>
2022-06-11 20:54:57 +0300

    a

---
+++ hw_13_jn_2022/7_2.py
@@ -0,0 +1,11 @@
+def f(s1 , s2, n):
+    if s1 + s2 >= 80:
+        if n == 3: return True
+        else: return False
+    else:
+        if n >= 3: return False
+        elif n % 2 != 0: return f(s1 + 1, s2, n + 1) and f(s1, s2 + 1, n + 1) and f(s1 + s2, s2, n + 1) and f(s1, s2 + s1, n + 1)
+        else: return f(s1 + 1, s2, n + 1) or f(s1, s2 + 1, n + 1) or f(s1 + s2, s2, n + 1) or f(s1, s2 + s1, n + 1)
+for s in range(1, 72):
+    if f(8, s, 0):
+        print(s)
