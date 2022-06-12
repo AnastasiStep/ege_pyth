ba1225010b4ca2ddd5b994d3c587157556445786
Alex Aredov <aaredov@bk.ru>
2022-06-12 16:30:52 +0300

    ds

--- hw_13_jn_2022/10.py
+++ hw_13_jn_2022/10.py
@@ -2,10 +2,24 @@ k = 0
 for i in range(166006, 167000, 10):
     if (i % 6 == 0) and (i % 7 == 0) and (i % 8 == 0):
         k += 1
-        print(i)
+        sd = []
+        for j in range(2, (int)(i**0.5)+1):
+            if (i % j == 0) and (j**2 != i):
+                sd.append(j)
+                sd.append(i//j)
+            if j**2 == i:
+                sd.append(j)
+        print(i, sum(sd))
 for i in range(1660006, 1670000, 10):
     if (i % 6 == 0) and (i % 7 == 0) and (i % 8 == 0):
         k += 1
-        print(i)
+        sd = []
+        for j in range(2, (int)(i**0.5)+1):
+            if (i % j == 0) and (j**2 != i):
+                sd.append(j)
+                sd.append(i//j)
+            if j**2 == i:
+                sd.append(j)
+        print(i, sum(sd))
     if k == 7:
         break
