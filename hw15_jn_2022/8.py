d467c091febad9d47c0c8fdd0239d028ea10cb63
Alex Aredov <59872895+AlexAredov@users.noreply.github.com>
2022-06-13 20:18:08 +0300

    2

---
+++ hw15_jn_2022/8.py
@@ -0,0 +1,7 @@
+def f(x, y):
+    if x == y: return 1
+    elif x > y: return 0
+    else:
+        #return f(x + 1, y) + f(int(str(x) + '1') if int(str(x) + '1') % 3 == 0 else x, y) + f(x * 5, y)
+        return f(x + 1, y) + f((int(str(x) + '1'), x)[x % 3 == 0], y) + f(x * 5, y)
+print(f(1, 140))
