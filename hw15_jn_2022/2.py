2b12f475966da7077cc4e4b6e2d304fb78703dbe
Alex Aredov <59872895+AlexAredov@users.noreply.github.com>
2022-06-13 19:01:41 +0300

    2

---
+++ hw15_jn_2022/2.py
@@ -0,0 +1,17 @@
+s = 'АЕЖЙМУ'
+k = 0
+l = 0
+for i1 in s:
+    for i2 in s:
+        for i3 in s:
+            for i4 in s:
+                for i5 in s:
+                    for i6 in s:
+                        k += 1
+                        if k % 2 == 0:
+                            w = i1 + i2 + i3 + i4 + i5 + i6
+                            if (w.count('А') == 1 and w.count('Е') == 1
+                            and w.count('Ж') == 1 and w.count('Й') == 1
+                            and w.count('М') == 1 and w.count('У') == 1):
+                                l += 1
+print(l)
