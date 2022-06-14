9599a70103519c13235c3551da0e3be5e1b5939f
Alex Aredov <aaredov@bk.ru>
2022-06-14 13:34:25 +0300

    d

---
+++ hw15_jn_2022/16.py
@@ -0,0 +1,20 @@
+f = open('hw15_jn_2022/16B.txt')
+l = int(f.readline())
+M = 4043520
+s = []
+for i in range(l):
+    s.append(int(f.readline()))
+sd = []
+for i in range(len(s)):
+    if M % s[i] == 0:
+        if [s[i]] not in sd:
+            sd.append([s[i]])
+            p = s[i]
+            while M % (p * s[i + 1]) == 0 and i < len(s) and p <= M:
+                d = [k for k in sd[i]]
+                d.append(s[i + 1])
+                sd.append(d)
+                p *= s[i + 1]
+                i += 1
+print(len(sd))
+        
