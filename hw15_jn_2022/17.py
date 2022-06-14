10557a3aff555213fbb3cae56f0147f49556b06d
Alex Aredov <aaredov@bk.ru>
2022-06-14 13:59:40 +0300

    fddg

---
+++ hw15_jn_2022/17.py
@@ -0,0 +1,16 @@
+f = open('hw15_jn_2022/17A.txt')
+l = int(f.readline())
+maxa = 0
+
+k = f.readlines()
+for i in range(len(k)):
+    k[i] = int(k[i])
+
+sd = []
+for i in range(len(k) - 1):
+    summ = k[i]
+    for j in range(i, len(k)):
+        summ = summ + k[j]
+        if summ % 93 == 0 and summ % 2 != 0:
+            sd.append(summ)
+print(max(sd))
